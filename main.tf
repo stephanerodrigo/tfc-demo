603d1bf5f24ceb5523b60a2af8f84247e7f378d9
stephane rodrigo <stephane.rodrigo@capgemini.com>
2021-02-26 16:21:58 +0100

    update code

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
 provider "aws" {
   region = var.aws_region
 }
+
+#Basic Example Using AMI Lookup
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "vpc_sro" {
+  tags = {
+    "Name" = "VPC_SRO_TST"
+  }
+}
+
+data "aws_subnet" "subnet_sro" {
+  availability_zone = "${data.aws_availability_zones.availability.names[0]}"
+  vpc_id = "${data.aws_vpc.vpc_sro.id}"
+}
+
+data "aws_security_group" "sg-sro"{
+  vpc_id = "${data.aws_vpc.vpc_sro.id}"
+
+  tags = {
+    Name = "SG_SRO_TST"
+  }
+}
+
+resource "aws_instance" "tfc-demo" {
+  count         = var.EC2_count
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  availability_zone = "${var.region}a"
+  subnet_id = "${aws_subnet.subnet_sro.id}"
+  vpc_security_group_ids = "${aws_security_group..id}"
+
+  tags = {
+    Name = "tfc-demo"
+  }
+}
