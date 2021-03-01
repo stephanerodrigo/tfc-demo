34896065c844f39181ede9a4d8257f1b6a085be4
stephanerodrigo <79470701+stephanerodrigo@users.noreply.github.com>
2021-03-01 10:18:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "aws_availability_zones" "available" {}
 
 
 data "aws_subnet" "subnet_sro" {
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = data.aws_availability_zones.available.names[1]
   vpc_id = data.aws_vpc.vpc_sro.id
 }
 
